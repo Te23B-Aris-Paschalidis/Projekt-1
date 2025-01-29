21c3a538f0b8f93e6f877ec0b36c907f9d08d441
Aris Paschalidis <aris.paschalidis@elev.ga.ntig.se>
2025-01-29 14:57:26 +0100

    CHOOSE YES OR NO TO ENTER THE DUNGEON

--- Projekt 1/Program.cs
+++ Projekt 1/Program.cs
@@ -4,9 +4,23 @@ string nameofuser = Console.ReadLine();
 Console.WriteLine($"Welcome {nameofuser}\n");
 // 
 Console.WriteLine("Do you want to enter the dungeon yes or no?");
-if (true)
+
+string en = Console.ReadLine().ToLower();
+
+while (en != "yes" && en != "no")
+{
+    Console.WriteLine("You have to write yes or no!");
+    en = Console.ReadLine().ToLower();
+}
+
+
+if (en == "yes")
 {
     Console.WriteLine("Welcome to the dungeon you are now in the waiting room");
+    else
+    {
+        Console.WriteLine("");
+    }
 }
 
 
