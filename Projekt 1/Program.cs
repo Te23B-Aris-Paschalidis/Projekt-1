2ab630b217f9ad68448f940b5634925c38d286b0
Aris Paschalidis <aris.paschalidis@elev.ga.ntig.se>
2025-01-29 14:40:12 +0100

    Started with the code asked the user for the name and took it in as information to use when I ask questions to the user. and I started if the user want to enter the room.

--- Projekt 1/Program.cs
+++ Projekt 1/Program.cs
@@ -1,1 +1,13 @@
-﻿static void 
+﻿// Asking the users name and taking in the information from the user
+Console.WriteLine("Hello player what should system call you?");
+string nameofuser = Console.ReadLine();
+Console.WriteLine($"Welcome {nameofuser}\n");
+// 
+Console.WriteLine("Do you want to enter the dungeon yes or no?");
+if (true)
+{
+    Console.WriteLine("Welcome to the dungeon you are now in the waiting room");
+}
+
+
+Console.ReadLine();
