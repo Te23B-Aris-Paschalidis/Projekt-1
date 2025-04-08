5f1c889b1dd1cb28aaab5496c5c746f68dcc0a3c
Aris Paschalidis <aris.paschalidis@elev.ga.ntig.se>
2025-04-08 13:26:24 +0200

    wewafafwa

--- Projekt 1/Program.cs
+++ Projekt 1/Program.cs
@@ -1,12 +1,10 @@
-﻿// Asking the users name and taking in the information from the user
+﻿
+
+// Asking the users name and taking in the information from the user
 Console.WriteLine("Hello player what should system call you?");
 string nameofuser = Console.ReadLine();
 Console.WriteLine($"Welcome {nameofuser}\n");
-
-// 
 Console.WriteLine("Do you want to enter the dungeon yes or no?");
-
-string weaponName = "";
 
 // A loop that force the person to say yes or no
 static string Loop()
@@ -25,37 +23,48 @@ string en = Loop();
 
 if (en == "yes")
 {
-    Console.WriteLine("Welcome to the dungeon you are now in the waiting room");
+    Console.WriteLine("\nWelcome to the dungeon you are now in the waiting room\n");
 }
-    else
-    {
-        Console.WriteLine($"{nameofuser} you don't have a choice you are forced to enter the room!");
-        Console.WriteLine();
-        Console.WriteLine($"{nameofuser} you are now in the waiting room");
-    }
-    // A list were you can choose your weapon
-    List<string> weapons = [];
-    
-    ChooseWeapon(weapons, "Sword", "Staff");
+else
+{
+    Console.WriteLine($"{nameofuser} you don't have a choice you are forced to enter the room!");
+    Console.WriteLine();
+    Console.WriteLine($"{nameofuser} you are now in the waiting room");
+    Console.WriteLine();
+
+}
+// A list were you can choose your weapon
+List<string> weapons = [];
+
+ChooseWeapon(weapons, "wooden sword", "wooden staff");
 
 
 Console.ReadLine();
 
 
-
+//  A method to let the user chose a weapon.
 
 static void ChooseWeapon(List<string> weapons, string weapon1, string weapon2)
 {
-    Console.WriteLine($"You have a choice between 1) {weapon1} and 2) {weapon2}");
+    Console.WriteLine($"You have a choice between: \n1) {weapon1} (5 physical dmg) \n2) {weapon2} (5 magic dmg)\n");
     Console.WriteLine("What do you choose?");
 
-    string wChoice = Console.ReadLine();
-    if (wChoice == "1" || wChoice == weapon1)
+    string wChoice = Console.ReadLine().ToLower();
+    if (wChoice == "1" || wChoice == weapon1 || wChoice == "sword")
     {
         weapons.Add(weapon1);
+        Console.WriteLine("You have chosen Wooden Sword");
     }
-    if (wChoice == "2" || wChoice == weapon2)
+    else if (wChoice == "2" || wChoice == weapon2)
     {
         weapons.Add(weapon2);
+        Console.WriteLine("You have chosen Wooden Staff");
+        Console.WriteLine();
     }
 }
+
+Console.WriteLine($"{nameofuser} you have 5 gold to spend on health potions do you want to use your gold now yes or no?");
+string a = Loop();
+Console.ReadLine();
+
+
